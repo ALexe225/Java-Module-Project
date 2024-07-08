8dc8f1a126e0fe2fefe83ddf5e77e5888d58d091
Alexe225 <den.pronin.91@gmail.com>
2024-07-08 16:14:00 +0300

    Первая ПР "Гонка 24"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,48 @@
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // ваш код начнется здесь
-        // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
-        System.out.println("Привет Мир");
+        Scanner scanner = new Scanner(System.in);
+        Race race = new Race();
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("Введите название машины №" + i);
+            String name = scanner.next();
+            System.out.println("Введите скорость машины №" + i);
+            while (true) {
+                int speed = scanner.nextInt();
+                if (speed > 0 & speed <= 250) {
+
+                    race.calculate(name, speed);
+                    break;
+                } else {
+                    System.out.println("Введено неверное значение. Попробуйте еще раз.");
+                }
+            }
+
+        }
+        System.out.println("Самая быстрая машина: " + race.leader);
+    }
+}
+class Car {
+    String name;
+    int speed;
+
+    public Car(String name, int speed) {
+        this.name = name;
+        this.speed = speed;
+    }
+}
+
+class Race {
+    String leader = "";
+    int leaderDistace = 0;
+
+    void calculate(String name, int speed) {
+        int distance;
+        distance = 24 * speed;
+        if (distance > leaderDistace) {
+            leader = name;
+            leaderDistace = distance;
+        }
     }
 }
